0ad46424f5ccffba08d56be595643a56928991f5
Henrique-dev-git <carlos.henrique.psic@gmail.com>
2022-07-19 20:50:12 -0300

    conclusão exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+let nome 
+let idade
+
+console.log (typeof nome,idade);
+
+//undefined pois não há valor atribuído.
+
+nome = prompt ("qual seu nome?")
+idade = prompt ("qual seu nome?")
+
+console.log (typeof nome,idade);
+
+//string pois tudo que vem de um prompt é uma string.
+
+console.log ("olá",nome,"você tem",idade,"anos")
+
+
+
+
