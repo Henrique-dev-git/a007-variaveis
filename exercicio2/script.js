1fe8a1abf6cb6026d1ce577d2b47af9dd0327afa
Henrique-dev-git <carlos.henrique.psic@gmail.com>
2022-07-19 20:50:41 -0300

    conclusão exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,20 @@
+let camisa
+let meias
+let chapeu
+
+camisa =prompt ("você está usando roupa azul?");
+meias =prompt ("você está usando meias?");
+chapeu =prompt ("você está usando chapeu de aba reta?");
+
+console.log (camisa);
+console.log (meias);
+console.log (chapeu);
+
+let perguntaA ="você está usando roupa azul?"
+let perguntaB ="você está usando meias?"
+let perguntaC ="você está usando chapeu de aba reta?"
+
+camisa =prompt (perguntaC);
+meias =prompt (perguntaA);
+chapeu =prompt (perguntaB);
+
